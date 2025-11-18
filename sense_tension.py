d2a91e44638ed70a318ff12ed6a6be7140a284dc
Lizzy <alyssra.chanpaibool@gmail.com>
2025-11-17 17:13:05 -0800

    Add comments/prints

--- sense_tension.py
+++ sense_tension.py
@@ -1,26 +1,27 @@
 """
-Hardware pipeline: Tension Sensor -> HX711 Amplifier (increase the signal) -> Raspberry Pi
+Hardware pipeline: Tension Sensor -> HX711 Amplifier -> Raspberry Pi
 """
 import gpio         # read GPIO pins
 import hx711        # access amplifier, HX711
 import time
 
-DT_PIN = 5          
-SCK_PIN = 6
+DAT_PIN = 5         # connected to DAT pin from HX711       
+CLK_PIN = 6         # connected to CLK pin from HX711
 
-CALIBRATION_FACTOR = 220.0  # adjust based on your measurement
+CALIBRATION_FACTOR = 220.0  # adjust based on measurement (how much to amplify)
 
-hx = hx711.HX711(dout_pin=DT_PIN, pd_sck_pin=SCK_PIN)
+hx = hx711.HX711(dout_pin=DAT_PIN, pd_sck_pin=CLK_PIN)
 hx.reset()
-hx.tare()
+hx.tare()           # set baseline
 
 print("Starting measurements...")
 
 try:
     while True:
         raw = hx.get_weight_mean(10)
+        print(f"Raw weight: {raw:.2f}")
         weight = raw / CALIBRATION_FACTOR
-        print(f"{weight:.2f} kg")
+        print(f"Calibrated Weight (kg): {weight:.2f}")
         time.sleep(0.2)
 
 except KeyboardInterrupt:
