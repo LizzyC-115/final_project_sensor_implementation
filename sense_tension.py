94577174bd5358f50416e50589223ae4abb7b01f
Lizzy Chanpaibool <achanpai@stanford.edu>
2025-12-01 14:47:54 -0800

    Refactor HX711 initialization and error handling

--- sense_tension.py
+++ sense_tension.py
@@ -1,32 +1,41 @@
+"""
+Used by Raspberry Pi Pico W to read load cell data.
+"""
 from machine import Pin
 import time
 from hx711 import HX711
-from picozero import pico_led
 
-# -----------------------------
-# Setup HX711 pins
-# -----------------------------
+
 DT_PIN = Pin(15, Pin.IN)   # DOUT
 SCK_PIN = Pin(14, Pin.OUT) # SCK
+BUFFER = 5                 # Threshold to send command
 
-hx = HX711(SCK_PIN, DT_PIN)
-hx.set_scale(1000)   # initial scale factor (calibrate later)
-hx.tare()            # tare the scale
+def retrieve(dt_pin, sck_pin, buffer):
+    hx = HX711(sck_pin, dt_pin)
+    hx.set_scale(1000)   # initial scale factor (calibrate later)
+    hx.tare()            # tare the scale
 
-print("Tare done. Start weighing...\n")
+    print("Tare done. Start weighing...\n")
 
-# -----------------------------
-# Main loop
-# -----------------------------
-try:
-#     pico_led.blink()
-    while True:
+
+    try: 
         weight = hx.get_units()
         print("Weight: {:.2f} g".format(weight))
-        time.sleep(0.2)
+        
+        if weight < -buffer:
+            print("[TURN LEFT]")
+        
+        elif weight > buffer:
+            print("[TURN RIGHT]")
+            
+        else:
+            print("SEND NO COMMANDS")
 
-except KeyboardInterrupt:
-    print("\nStopped by user.")
+    time.sleep(0.2)
+            
+    except Exception as e:
+        print(f"Error reading sensor: {e}")
+
 
 
 
