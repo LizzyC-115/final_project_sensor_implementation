92657219313476cdfd36e6a1b42083746ed2584f
Lizzy <alyssra.chanpaibool@gmail.com>
2025-12-01 16:47:15 -0800

    Add uart write

--- retrieve_data.py
+++ retrieve_data.py
@@ -1,4 +1,4 @@
-from machine import Pin
+from machine import Pin, UART
 import time
 from hx711 import HX711
 
@@ -8,6 +8,7 @@ from hx711 import HX711
 DT_PIN = Pin(2, Pin.IN)   # DOUT
 SCK_PIN = Pin(3, Pin.OUT) # SCK
 
+uart = UART(0, baudrate=115200, tx=0, rx=1)
 hx = HX711(SCK_PIN, DT_PIN)
 hx.set_scale(1000)   # initial scale factor (calibrate later)
 hx.tare()            # tare the scale
@@ -21,6 +22,7 @@ try:
     while True:
         weight = hx.get_units()
         print("Weight: {:.2f} g".format(weight))
+        uart.write("Weight: {:.2f} g".format(weight))
         time.sleep(0.2)
 
 except KeyboardInterrupt:
--- sense_tension.py
+++ sense_tension.py
@@ -1,7 +1,7 @@
 """
 Used by Raspberry Pi Pico W to read load cell data.
 """
-from machine import Pin
+from machine import Pin, UART
 import time
 from hx711 import HX711
 
@@ -14,27 +14,32 @@ def retrieve(dt_pin, sck_pin, buffer):
     hx = HX711(sck_pin, dt_pin)
     hx.set_scale(1000)   # initial scale factor (calibrate later)
     hx.tare()            # tare the scale
-
+    uart = UART(0, baudrate=115200, tx=0, rx=1)
     print("Tare done. Start weighing...\n")
-
+    uart.write("Tare done. Start weighing...\n")
 
     try: 
         weight = hx.get_units()
         print("Weight: {:.2f} g".format(weight))
-        
+        uart.write("Weight: {:.2f} g".format(weight))
+
         if weight < -buffer:
             print("[TURN LEFT]")
+            uart.write("[TURN LEFT]")
         
         elif weight > buffer:
             print("[TURN RIGHT]")
+            uart.write("[TURN RIGHT]")
             
         else:
-            print("SEND NO COMMANDS")
+            print("[]")
+            uart.write("[]")
 
     time.sleep(0.2)
             
     except Exception as e:
         print(f"Error reading sensor: {e}")
+        uart.write(f"Error reading sensor: {e}")
 
 
 
